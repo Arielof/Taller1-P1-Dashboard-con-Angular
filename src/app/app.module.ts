72ab2838c4663c0096831742cfb0b9d45e7804a8
Arielof <aop.floril@yavirac.edu.ec>
2022-11-21 17:26:28 -0600

    Proyecto dash

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { UserComponent } from './pages/user/user.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HeaderComponent } from './static/header/header.component';
+import { FooterComponent } from './static/footer/footer.component';
+import { SidebarComponent } from './static/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    AboutComponent,
+    UserComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,    
+    DashboardComponent,
+    NotFoundComponent,
+    LoginComponent,
+    BreadcrumbsComponent
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
